92151eaf13a66a77bec00d9a94cb7fbe2a80173e
Tobias Melbø <142866886+tobiasmelbo@users.noreply.github.com>
2024-06-08 16:45:04 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 $(document).ready(function() {
-    /*Setter opp lys/mørk modus fra enhetsinnstillinger*/
+    /* Setter opp lys/mørk modus fra enhetsinnstillinger */
     if (window.matchMedia && window.matchMedia("(prefers-color-scheme: light)").matches) {
         $("body").addClass("light-mode")
     }
+
+    /* Henter lengden av spillfilen fra https://github.com/tobiasmelbo/om_meg/raw/main/list.csv */
+    $.ajax({
+        url: "https://raw.githubusercontent.com/tobiasmelbo/om_meg/main/list.csv",
+        dataType: "text",
+        success: function(data){
+            $("#antall_spill").text(data.split("\n").length)
+        },
+        error: function(){
+            console.log("Feil ved henting av spilldata.")
+        }
+    })
 })
 
 function toggle_theme() {
